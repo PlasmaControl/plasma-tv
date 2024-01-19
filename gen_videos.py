d9a9c9057f65ccfcd2fd39667b94dba94b19d311
Nathaniel Chen <79379842+nathanchenseanwalter@users.noreply.github.com>
2024-01-19 13:20:46 -0800

    some edits

--- gen_videos.py
+++ gen_videos.py
@@ -3,9 +3,15 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from pathlib import Path
 from scipy.io import readsav
+import matplotlib.gridspec as gridspec
 from tqdm import tqdm
 import h5py
 import cv2
+
+def format_axes(fig):
+    for i, ax in enumerate(fig.axes):
+        ax.text(0.5, 0.5, "ax%d" % (i+1), va="center", ha="center")
+        ax.tick_params(labelbottom=False, labelleft=False)
 
 def gen_reg_vids():
     tv_image_path = Path('tv_images')
@@ -20,27 +26,27 @@ def gen_reg_vids():
     w, h = width, tv_dim[0] / tv_dim[1] * width
     for file in files:
         
-        frames = readsav(file)['emission_structure'][0][3].astype(int)
-        tv_image = readsav(file)['emission_structure'][0][7][frames]
-        num_frames = len(frames)
-        
-
+        tv_image = readsav(file)['emission_structure'][0][7]
+        tv_times = readsav(file)['emission_structure'][0][6]
+   
         # Create the figure and axes
-        fig, axes = plt.subplots(frameon=False)
+        fig, axes = plt.subplots(1, 1)
 
         # Initialize the image
-        image1 = axes.imshow(tv_image[0], cmap='plasma')
+        image1 = axes.imshow(tv_image[0], cmap='plasma', vmin=0, vmax=255)
         axes.axis('off')
-        fig.set_size_inches(w,h)
-        fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
-        
+        plt.suptitle(f'{file.stem}')
+        fig.set_size_inches(w,h * 1.7)
+        fig.subplots_adjust(left=0, bottom=0.05, right=1, top=0.95, wspace=None, hspace=None)
+        fig.colorbar(image1, ax=axes, orientation='horizontal', pad=0.01, shrink=0.8)
         # Define the update function for the animation
         def update(frame):
-            image1.set_array(tv_image[frame])
+            image1.set_array(np.flip(tv_image[frame],1))
+            axes.set_title(f'Time: {tv_times[frame]:.2e} ms, Frame: {frame}')
             return image1,
 
         # Create the animation
-        ani = animation.FuncAnimation(fig, update, frames=tqdm(range(num_frames)), interval=25, blit=True)
+        ani = animation.FuncAnimation(fig, update, frames=tqdm(range(len(tv_image))), interval=30, blit=True)
 
         # Save the animation to a file
         output_file = out_path / Path(file.stem).with_suffix('.mp4')
@@ -58,24 +64,31 @@ def gen_comparison_vids():
 
     for file in tqdm(files):
         
-        frames = readsav(file)['emission_structure'][0][3].astype(int)
-        tv_image = readsav(file)['emission_structure'][0][7][frames]
+        _frames = readsav(file)['emission_structure'][0][3].astype(int)
+        tv_image = readsav(file)['emission_structure'][0][7][_frames] # get only images corresponding to inverted
+        tv_times = readsav(file)['emission_structure'][0][6][_frames]
         inversion_image = readsav(file)['emission_structure'][0][0]
-        num_frames = len(frames)
+        num_frames = len(_frames)
 
         # Create the figure and axes
-        fig, axes = plt.subplots(2, 1, figsize=(5, 10), gridspec_kw={'width_ratios': [1]})
-        fig.tight_layout()
+        fig = plt.figure()
+        
+        gs0 = gridspec.GridSpec(2, 1, figure=fig)
+
+        ax1 = fig.add_subplot(gs0[0])
+        ax2 = fig.add_subplot(gs0[1])
 
         # Initialize the images
-        image1 = axes[0].imshow(tv_image[0], cmap='plasma')
-        image2 = axes[1].imshow(inversion_image[0], cmap='viridis', origin='lower')
-
+        image1 = ax1.imshow(tv_image[0], cmap='plasma')
+        image2 = ax2.imshow(inversion_image[0], cmap='viridis', origin='lower', aspect='auto')
+        title = ax1.set_title(f'{file.stem}')
+        plt.suptitle(f'{file.stem}')
         # Define the update function for the animation
         def update(frame):
-            image1.set_array(tv_image[frame])
+            image1.set_array(np.flip(tv_image[frame],1))
             image2.set_array(inversion_image[frame])
-            return image1, image2
+            title.set_text(f'Time: {tv_times[frame]:.2e} ms, Frame: {_frames[frame]}')
+            return image1, image2, title
 
         # Create the animation
         ani = animation.FuncAnimation(fig, update, frames=tqdm(range(num_frames)), interval=25, blit=True)
